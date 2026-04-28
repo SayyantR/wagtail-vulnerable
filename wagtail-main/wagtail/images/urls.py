0c36457a6e68c62d3843e606021e6cd473feb56d
SayyantR <sayyant@semgrep.com>
2026-04-28 13:36:54 -0700

    Added sqli vuln

--- wagtail-main/wagtail/images/urls.py
+++ wagtail-main/wagtail/images/urls.py
@@ -1,7 +1,9 @@
-from django.urls import re_path
+from django.urls import path, re_path
 
+from wagtail.images.views.preview_filter import ColorFormulaPreviewView
 from wagtail.images.views.serve import serve
 
 urlpatterns = [
     re_path(r"^([^/]*)/(\d*)/([^/]*)/[^/]*$", serve, name="wagtailimages_serve"),
+    path("<int:image_id>/preview-filter/", ColorFormulaPreviewView.as_view(), name="wagtailimages_preview_filter"),
 ]
